d6e1eaeccbb5b95dea654176edf9bfef024f026a
MohammedBaqerAdnan <sayedmohammedadnanbiz@gmail.com>
2023-11-04 00:51:25 +0300

    sample test foy api meta using python

--- lib/TestSendRecieveMessages.py
+++ lib/TestSendRecieveMessages.py
@@ -2,9 +2,9 @@ import json
 import requests
 
 # Define your phone number ID, access token, and the recipient's phone number
-phone_number_id = "163959690131871" # Phone number ID provided
-access_token = "EAAKL40FZATe4BOx9UgpKNK5BZCioJ9TCcbWhvZBNsX8zrefh6zsbcVCb1AIMhzLQgw2GAoefnvfmRSUTn0dL045EmJoL8iVtMeWNb61qeIF6fdkjZA94fYdX0KoHfOQsllINlYBISSUlG5zvTwVmsawysHewqLq5cgy99Q2pYoQoBS8pGzz4z8Bg88rabzFBPA9aOJomVJRwZCPZBwsOkZD" # Your temporary access token
-recipient_phone_number = "97334466419" # Your own phone number
+phone_number_id = "169999262855512" # Phone number ID provided
+access_token = "EAAFN7IE6TUABO4qp6rFaAzaSaHkKogeJ5TSnW6kR3TSMMTPcZBWg3zDvc0kNCsQevKgzMgJOWZAJwbAsureTBdxEim0ZBV8eVDgklUp18ivHky5Vtd3QJ43Y5WGWKVuRiYZC4CEO5kh2CXGxcrQJTPemBifsGhNuh8iVUgy4Qzczah0JE6JT7TKzvffwhd5ljGJGH53tBOyEnWA6YZBDl" # Your temporary access token
+recipient_phone_number = "97333407786" # Your own phone number
 
 # Define the URL for the POST HTTP request and the headers for this request
 url = f"https://graph.facebook.com/v17.0/{phone_number_id}/messages"
