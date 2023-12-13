570f67f58a8a17a1a5a0d5abfc90c91d15a3719e
MohammedBaqerAdnan <sayedmohammedadnanbiz@gmail.com>
2023-12-13 11:13:07 +0300

    Remove commented out code for Vigenere encryption

--- lib/EncryptionUtils.dart
+++ lib/EncryptionUtils.dart
@@ -1,32 +1,3 @@
-// //function take text as string to be encrypted or decrypted , key for vigenere to add it with text , encrypt flag for (1-encrypted ,0-decrypted)
-// String vigenere(String text, String key, int encrypt) {
-// //list to stores ascii code of key and text
-//   List<int> textCodeUnits = text.codeUnits;
-//   List<int> keyCodeUnits = key.codeUnits;
-//   String result = '';
-// //index for the key list
-//   int j = 0;
-//   for (var i = 0; i < text.length; i++) {
-//     if (encrypt == 1) {
-//       // Encryption: (text + key) % 26 + 65 why 65 because A start from 65 in ascii
-//       int x = (textCodeUnits[i] + keyCodeUnits[j]) % 26 + 65;
-// //convert back to character using ascii
-//       result += String.fromCharCode(x);
-//     } else {
-//       // Decryption:
-//       int y = ((textCodeUnits[i] - keyCodeUnits[j]) % 26 + 26) % 26;
-//       result += String.fromCharCode(y + 65);
-//     }
-// //if iteration end rest otherwise increment j to go over the k
-//     if (j < key.length - 1) {
-//       j++;
-//     } else {
-//       j = 0;
-//     }
-//   }
-//   return result;
-// }
-
 String vigenere(String inputText, String key, int encrypt) {
   String result = '';
   var j = 0;
